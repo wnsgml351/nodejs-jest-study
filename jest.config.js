c5ac6c00c0dca6b47207c150f26b29a6cbab98eb
lee jun hee <wnsgml351@naver.com>
2024-10-08 15:33:03 +0900

    타입스크립트 세팅 추가

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
+// export default {
+//   testEnvironment: "node",
+//   transform: {
+//     "^.+.tsx?$": ["ts-jest",{}],
+//   },
+// };
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+}
--- src/toBe.spec.ts
+++ src/toBe.spec.ts
@@ -1,4 +1,4 @@
-import sum from './toBe';
+import { sum } from './toBe';
 
 test('sum 함수는 두 숫자를 더해야한다', () => {
     expect(sum(1,2)).toBe(3);
