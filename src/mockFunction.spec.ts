a1e4ececd9b4db244a2651370fa121e66743d870
LEE JUN HEE <wnsgml351@naver.com>
2024-10-09 21:13:32 +0900

    테스트 라이프 사이클로 중복 코드 제거

--- src/mockFunction.spec.ts
+++ src/mockFunction.spec.ts
@@ -1,7 +1,8 @@
 import { obj } from './mockFunction';
 
 test('obj.minus 함수가 1번 호출되었다 (spy 삽입)', () => {
-    const spyFn = jest.spyOn(obj, 'minus');
+    // const spyFn = jest.spyOn(obj, 'minus');
+    jest.spyOn(obj, 'minus');
     const result = obj.minus(1, 2);
     expect(obj.minus).toHaveBeenCalledTimes(1);
     expect(result).toBe(-1);
@@ -9,28 +10,25 @@ test('obj.minus 함수가 1번 호출되었다 (spy 삽입)', () => {
     spyFn.mockClear(); // Times, With 초기화
     spyFn.mockReset(); // mockClear + mockImplementation(() => {})
     */
-    spyFn.mockRestore(); // 아예 전부 없애버림
+    // spyFn.mockRestore(); // 아예 전부 없애버림
 });
 
 test('obj.minus에 스파이를 심고 실행도 안되게', () => {
-    const spyFn = jest.spyOn(obj, 'minus').mockImplementation();
+    jest.spyOn(obj, 'minus').mockImplementation();
     const result = obj.minus(1, 2);
     expect(obj.minus).toHaveBeenCalledTimes(1);
     expect(result).not.toBe(-1);
-    spyFn.mockRestore();
 });
 
 test('obj.minus에 스파이를 심고 리턴값을 바꾸게', () => {
-    const spyFn = jest.spyOn(obj, 'minus').mockImplementation((a, b) => a + b);
+    jest.spyOn(obj, 'minus').mockImplementation((a, b) => a + b);
     const result = obj.minus(1, 2);
     expect(obj.minus).toHaveBeenCalledTimes(1);
     expect(result).toBe(3);
-    spyFn.mockRestore();
 });
 
 test('obj.minus에 스파이를 심고 리턴값이 서로 다르게 나오게', () => {
-    const spyFn = jest
-        .spyOn(obj, 'minus')
+    jest.spyOn(obj, 'minus')
         .mockImplementationOnce((a, b) => a + b)
         .mockImplementationOnce(() => 5);
     const result1 = obj.minus(1, 2);
@@ -40,19 +38,19 @@ test('obj.minus에 스파이를 심고 리턴값이 서로 다르게 나오게',
     expect(result1).toBe(3);
     expect(result2).toBe(5);
     expect(result3).toBe(-1);
-    spyFn.mockRestore();
+    // spyFn.mockRestore();
 });
 
 test('obj.minus에 스파이를 심고 리턴값이 서로 다르게 나오게(mockReturnValue)', () => {
-    const spyFn = jest.spyOn(obj, 'minus').mockReturnValue(5);
+    jest.spyOn(obj, 'minus').mockReturnValue(5);
     const result1 = obj.minus(1, 2);
     expect(obj.minus).toHaveBeenCalledTimes(1);
     expect(result1).toBe(5);
-    spyFn.mockRestore();
+    // spyFn.mockRestore();
 });
 
 test('obj.minus에 스파이를 심고 리턴값이 서로 다르게 나오게(mockReturnValueOnce)', () => {
-    const spyFn = jest.spyOn(obj, 'minus').mockReturnValueOnce(5).mockReturnValueOnce(3).mockReturnValue(8);
+    jest.spyOn(obj, 'minus').mockReturnValueOnce(5).mockReturnValueOnce(3).mockReturnValue(8);
     const result1 = obj.minus(1, 2);
     const result2 = obj.minus(1, 2);
     const result3 = obj.minus(1, 2);
@@ -60,5 +58,21 @@ test('obj.minus에 스파이를 심고 리턴값이 서로 다르게 나오게(m
     expect(result1).toBe(5);
     expect(result2).toBe(3);
     expect(result3).toBe(8);
-    spyFn.mockRestore();
+});
+
+beforeAll(() => {
+    console.log('이 파일의 준비사항 실행');
+});
+
+beforeEach(() => {
+    console.log('각 테스트 전에 실행됨');
+});
+
+afterEach(() => {
+    console.log('각 테스트 후에 실행됨');
+    jest.restoreAllMocks();
+});
+
+afterAll(() => {
+    console.log('모든 테스트가 끝난 후');
 });
